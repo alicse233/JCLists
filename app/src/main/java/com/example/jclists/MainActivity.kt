c7b2653fb95466ad550c3d23b505a8e065b4a840
Ali <ali.cse233@gmail.com>
2021-12-23 14:33:04 +0300

    Updated with checkbox
    Lazy column item is updated with a checkbox

--- app/src/main/java/com/example/jclists/MainActivity.kt
+++ app/src/main/java/com/example/jclists/MainActivity.kt
@@ -8,10 +8,15 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Checkbox
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -27,7 +32,7 @@ class MainActivity : ComponentActivity() {
             JCListsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    ListsWithRecycling()
+                    ListsWithoutRecycling()
                 }
             }
         }
@@ -44,15 +49,27 @@ fun ListsWithoutRecycling() {
     ) {
 
         for (i in 1..50) {
-            Text(
-                text = "Item $i",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(12.dp),
-                textAlign = TextAlign.Center,
-                color = Color.Black,
-                fontWeight = FontWeight.Bold
-            )
+            var stateCB = remember {
+                mutableStateOf(false)
+            }
+
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .align(CenterHorizontally)) {
+                Checkbox(checked = stateCB.value, onCheckedChange = {
+                    stateCB.value = it
+                }, Modifier.padding(12.dp))
+
+                Text(
+                    text = "Item $i",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(12.dp),
+                    textAlign = TextAlign.Start,
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold
+                )
+            }
         }
     }
 }
